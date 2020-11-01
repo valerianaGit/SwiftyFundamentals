82b4c79ed3a72f668b5491d145f18360157cea00
Valeria Duran de Ruiz <valeria.do@gmail.com>
2020-11-01 15:01:04 -0600

    create documentation for hash table file

--- SwiftyFundamentals/SwiftyFundamentals/DataStructures/HashTable.swift
+++ SwiftyFundamentals/SwiftyFundamentals/DataStructures/HashTable.swift
@@ -1,14 +1,17 @@
 //
-//  MappingAndHashing.swift
+//  HashTable.swift
 //  SwiftyFundamentals
 //
 //  Created by Valeria Duran Ruiz on 11/1/20.
 //  Copyright © 2020 Valeria Duran Ruiz. All rights reserved.
 //
+// This file is intended to showcase the Hash table data structure
+// how to implement it in Swift
+// cool features you can use with
 
 import Foundation
 
-class MappingAndHashing {
+class HashTable {
     
     func whatIsMapAnyway() {
         
