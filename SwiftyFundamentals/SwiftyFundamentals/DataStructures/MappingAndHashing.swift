4c4897f426142072a3c617c412d021cca99970b1
Valeria Duran de Ruiz <valeria.do@gmail.com>
2020-11-01 14:58:40 -0600

    create map and hash file

---
+++ SwiftyFundamentals/SwiftyFundamentals/DataStructures/MappingAndHashing.swift
@@ -0,0 +1,20 @@
+//
+//  MappingAndHashing.swift
+//  SwiftyFundamentals
+//
+//  Created by Valeria Duran Ruiz on 11/1/20.
+//  Copyright © 2020 Valeria Duran Ruiz. All rights reserved.
+//
+
+import Foundation
+
+class MappingAndHashing {
+    
+    func whatIsMapAnyway() {
+        
+    }
+    
+    func createMyFirstMap() {
+        
+    }
+}
