4dd80bb5f93b5107ab9a4bad614cf6c824709a46
Valeria Duran de Ruiz <valeria.do@gmail.com>
2020-11-01 14:56:23 -0600

    create xcode project

---
+++ SwiftyFundamentals/SwiftyFundamentals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftyFundamentals
+//
+//  Created by Valeria Duran Ruiz on 11/1/20.
+//  Copyright © 2020 Valeria Duran Ruiz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
